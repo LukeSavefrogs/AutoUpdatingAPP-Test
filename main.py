b1684086c4898df434e968d7638607392fedacad
LukeSavefrogs-Desktop-Work <lucasalvarani99@gmail.com>
2020-11-19 06:26:17 +0100

    Fix Version

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from justupdate.client.client import JustUpdateClient
 from client_config import ClientConfig
 
 if __name__ == "__main__":
-	client = JustUpdateClient(ClientConfig(), "0.0.2", "stable")
+	client = JustUpdateClient(ClientConfig(), "0.0.3", "stable")
 
 	if client.update_available():
 		print("An update is available!")
