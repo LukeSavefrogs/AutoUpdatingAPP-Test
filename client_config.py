c83c5cc85ff284863aa711624b24ba1518cc1d7d
LukeSavefrogs-Desktop-Work <lucasalvarani99@gmail.com>
2020-12-02 20:00:31 +0100

    Tried following issue #5

--- client_config.py
+++ client_config.py
@@ -1,4 +1,4 @@
 class ClientConfig():
 	app_name = "AutoUpdatingAPP-Test"
 	app_author = "Luca Salvarani"
-	update_url = "https://github.com/LukeSavefrogs/AutoUpdatingAPP-Test/deployedFiles"
+	update_url = "https://https://raw.githubusercontent.com/LukeSavefrogs/AutoUpdatingAPP-Test/main/deployedFiles/"
